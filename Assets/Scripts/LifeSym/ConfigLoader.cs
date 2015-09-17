453698a6f5f9b7fb20a63f01c2728a28d021ba75
mrev1l1 <ds.noob11@gmail.com>
2015-09-17 04:08:26 +0300

    LifeSimulation
    Verson 0.1 without external dependencies

---
+++ Assets/Scripts/LifeSym/ConfigLoader.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ConfigLoader{
+
+	public string configFile { get; private set; }
+
+	public Dictionary<ArrayList,Vector3> timeTable { get; private set; }
+	public Dictionary<Vector3, float> randomPlaces { get; private set; }
+
+	public float satietyFactor { get; private set; }
+	public float moodFactor { get; private set; }
+	public float hygieneFactor { get; private set; }
+
+	public Vector3 satietyLocation { get; private set; }
+	public Vector3 moodLocation { get; private set; }
+	public Vector3 hygieneLocation { get; private set; }
+
+	public bool isReady { get; private set; }
+
+	public ConfigLoader(string path)
+	{
+		configFile = path;
+	}
+
+	// Use this for initialization
+	public void Initialise () 
+	{
+		this.randomPlaces = new Dictionary<Vector3, float> ();
+		this.timeTable = new Dictionary<ArrayList, Vector3> ();
+		this.configFile = System.IO.File.ReadAllText (configFile);
+
+		this.InitializetimeTable();
+		this.InitializeFactors ();
+		this.InitializerandomPlaces ();
+		this.InitializeLocations ();
+
+		this.isReady = true;
+	}
+
+	void InitializeLocations()
+	{
+		int startIndex = configFile.IndexOf ("SL");
+		int endIndex = configFile.IndexOf (";");
+
+		var locationSubstr = configFile.Substring (startIndex + 3, endIndex - startIndex - 3);
+		var locationList = locationSubstr.Split (',');
+
+		satietyLocation = new Vector3 (float.Parse (locationList [0]), float.Parse (locationList [1]), float.Parse (locationList [2]));
+
+		startIndex = configFile.IndexOf ("ML");
+		endIndex = configFile.IndexOf ("HL");
+
+		locationSubstr = configFile.Substring (startIndex + 3, endIndex - startIndex - 3 - 3);
+		locationList = locationSubstr.Split (',');
+
+		moodLocation = new Vector3 (float.Parse (locationList [0]), float.Parse (locationList [1]), float.Parse (locationList [2]));
+
+		startIndex = endIndex;
+		endIndex = configFile.IndexOf ("//");
+
+		locationSubstr = configFile.Substring (startIndex + 3, endIndex - startIndex - 3 - 3);
+		locationList = locationSubstr.Split (',');
+
+		hygieneLocation = new Vector3 (float.Parse (locationList [0]), float.Parse (locationList [1]), float.Parse (locationList [2]));
+	}
+
+	void InitializeFactors()
+	{
+		int startIndex = configFile.IndexOf ("SF");
+		int endIndex = configFile.IndexOf ("RP");
+
+		string factorString = configFile.Substring (startIndex, endIndex - startIndex);
+		endIndex = factorString.LastIndexOf ("\r\n");
+		configFile = configFile.Substring (startIndex + endIndex + 2, configFile.Length - endIndex - startIndex - 2);
+
+		startIndex = 0;
+		endIndex = factorString.IndexOf (";");
+		satietyFactor = float.Parse(factorString.Substring (startIndex + 3, endIndex - startIndex - 3));
+
+		factorString = factorString.Substring (endIndex + 1, factorString.Length - endIndex - 1);
+		startIndex = 0;
+		endIndex = factorString.IndexOf (";");
+		moodFactor = float.Parse (factorString.Substring(startIndex + 5, endIndex - startIndex - 5));
+
+		factorString = factorString.Substring (endIndex + 1, factorString.Length - endIndex - 1);
+		startIndex = 0;
+		endIndex = factorString.IndexOf (";");
+		hygieneFactor = float.Parse (factorString.Substring(startIndex + 5, endIndex - startIndex - 5));
+
+	}
+
+	void InitializerandomPlaces()
+	{
+		int startIndex = 0;
+		int endIndex = configFile.IndexOf ("RP");
+
+		int randomPlacesCount = int.Parse(configFile.Substring(startIndex, endIndex - startIndex));
+
+		startIndex = configFile.IndexOf("{") + 1;
+		while (randomPlacesCount > 0) 
+		{
+			endIndex = configFile.IndexOf(";");
+
+			var positionList = configFile.Substring(startIndex + 2, endIndex - startIndex - 2).Split(',');
+			Vector3 position = new Vector3(float.Parse(positionList[0]), float.Parse(positionList[1]), float.Parse(positionList[2]));
+
+			float chance = float.Parse(configFile.Substring(endIndex + 1, configFile.IndexOf(" ") - endIndex - 1));
+
+			randomPlaces.Add(position, chance);
+
+			configFile = configFile.Substring(configFile.IndexOf(" "), configFile.Length - configFile.IndexOf(" "));
+			configFile = configFile.Substring(configFile.IndexOf("\r\n"), configFile.Length - configFile.IndexOf("\r\n"));
+
+			randomPlacesCount--;
+			startIndex = 0;
+		}
+	}
+
+	void InitializetimeTable()
+	{
+		int startIndex = 0;
+		int endIndex= configFile.IndexOf ("P");
+		int positionCount = int.Parse(configFile.Substring (startIndex, endIndex - startIndex));
+		
+		startIndex = configFile.IndexOf ("\r\n");
+		endIndex = configFile.IndexOf ("}");
+		string timetableString = configFile.Substring(startIndex + 2, endIndex - startIndex - 2);
+
+		configFile = configFile.Substring (endIndex + 2, configFile.Length - endIndex - 2);
+
+		while (positionCount > 0)
+		{
+			startIndex = 0;
+			endIndex = timetableString.IndexOf("=");
+
+			var RangeSubstr = timetableString.Substring (startIndex, endIndex - startIndex);
+			var RangeList = RangeSubstr.Split (',');
+
+			ArrayList hourRange = new ArrayList ();
+
+			for (int i = 0; i < RangeList.Length; i++)
+			{
+				hourRange.Add (int.Parse (RangeList [i]));
+			}
+
+			startIndex = timetableString.IndexOf (";");
+			RangeSubstr = timetableString.Substring (endIndex + 1, startIndex - endIndex - 1);
+			RangeList = RangeSubstr.Split (',');
+
+			Vector3 position = new Vector3 (float.Parse (RangeList [0]), float.Parse (RangeList [1]), float.Parse (RangeList [2]));
+
+			timeTable.Add(hourRange,position);
+			positionCount--;
+			startIndex = timetableString.IndexOf("\r\n");
+			timetableString = timetableString.Substring(startIndex + 2, timetableString.Length - startIndex - 2);
+
+		}
+	}
+}
---
+++ Assets/Scripts/LifeSym/SimulationEngine.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;	
+using System.Collections;
+
+public class SimulationEngine : MonoBehaviour {
+
+	//  TODO: Путь к файлу нужно получать извне.
+	public ConfigLoader loader = new ConfigLoader("D:\\Zlato\\NPCLifestyle\\npc_config.txt"); 
+
+	public short satiety { get; private set; }
+	public short mood { get; private set; }
+	public short hygiene { get; private set; }
+
+	public GameObject player;
+	public GameObject thisNPC; //Подразумевается, что будет задаваться в Юнити вручную level-дизайнерами
+
+	private bool isCharging;
+	private float chargingTime = 0; //JUST FOR DEBBUGING
+	private float elapsedTime = 0; //JUST FOR DEBBUGING
+
+	private static int fatalMinimum = 9;
+	private static float bigDistance = 100.0f;
+	// Use this for initialization
+	void Start () {
+
+		loader.Initialise ();
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		satiety = (short)Random.Range (1, 24);
+		mood = (short)Random.Range (1, 24);
+		hygiene = (short)Random.Range (1, 24);
+
+		foreach (var happening in loader.randomPlaces)
+		{
+			if(Random.Range(0.0f, 1.0f) <= happening.Value)
+			{
+				print("go to "+ happening.Key);
+			}
+			else
+			{
+				print("don't go to "+ happening.Key);
+			}
+		}
+
+		foreach (var place in loader.timeTable) 
+		{
+			if(place.Key.Contains( 18 /* тут, по идее, нужно из объекта игрока брать время суток */))
+			{
+				thisNPC.transform.position = place.Value;
+				break;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (IsNear()) 
+		{
+			if(false /* проверка на то, что NPC не взаимодействует с игроком прямо сейчас,
+			          например не в диалоге \ драке*/)
+			{
+
+			}
+			else
+			{
+				if(!isCharging)
+				{
+					if(satiety <= mood && satiety <= hygiene && satiety < SimulationEngine.fatalMinimum)
+					{
+						thisNPC.transform.position = loader.satietyLocation; // пойти поесть. в будущем заменить на перемещение, 
+																			 // как оно будет готово
+						// запомнить время, когда ушел, чтобы час отсчитать
+						isCharging = true;
+						satiety = 24; print("need food!");
+						return;
+					}
+					if(mood <= satiety && mood <= hygiene && mood < SimulationEngine.fatalMinimum)
+					{
+						thisNPC.transform.position = loader.moodLocation;
+						isCharging = true;
+						mood = 24; print("need mood!");
+						return;
+					}
+					if(hygiene <= satiety && hygiene <= mood && hygiene < SimulationEngine.fatalMinimum)
+					{
+						thisNPC.transform.position = loader.hygieneLocation;
+						isCharging = true;
+						hygiene = 24; print("need washing :(!");
+						return;
+					}
+
+					//если прошел час, - уменьшить характеристики
+					elapsedTime+=Time.deltaTime;
+					if(elapsedTime > 10)
+					{
+						if(satiety > 0)
+						{
+							satiety -= (short)(1 * loader.satietyFactor);
+						}
+						if(mood > 0)
+						{
+							mood -= (short)(1 * loader.moodFactor);
+						}
+						if(hygiene > 0)
+						{
+							hygiene -= (short)(1 * loader.hygieneFactor);
+						}
+
+						elapsedTime = 0;
+
+						print("S: " + satiety +" M: " + mood + " H: " + hygiene);
+					}
+				}
+				else
+				{
+					chargingTime += Time.deltaTime;
+
+					if(chargingTime > 10)
+					{
+						isCharging = false;
+
+						foreach (var place in loader.timeTable) 
+						{
+							if(place.Key.Contains( 18 /* тут, по идее, нужно из объекта игрока брать время суток */))
+							{
+								thisNPC.transform.position = place.Value;
+								break;
+							}
+						}
+
+						chargingTime = 0;
+					}
+				}
+			}
+		
+		} else 
+		{
+			foreach(var place in loader.timeTable)
+			{
+				if(place.Key.Contains( 18 /* тут, по идее, нужно из объекта игрока брать время суток */))
+				{
+				   thisNPC.transform.position = place.Value;
+					break;
+				}
+			}
+		}
+	}
+
+	bool IsNear()
+	{
+		Vector3 distanceVector = new Vector3 (player.transform.position.x - thisNPC.transform.position.x, 
+		                                      player.transform.position.y - thisNPC.transform.position.y, 
+		                                      player.transform.position.z - thisNPC.transform.position.z);
+
+		float distance = Mathf.Sqrt (distanceVector.x * distanceVector.x+
+		                             distanceVector.y * distanceVector.y+
+		                             distanceVector.z * distanceVector.z);
+
+		if (distance <= SimulationEngine.bigDistance)
+			return true;
+
+		return false;
+	}
+}
